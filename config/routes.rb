afb76674af93c5c5c6d11f7b5288a2e075b2fbe9
Tyson Puetz <tpuetz@gmail.com>
2014-01-17 11:10:56 -0500

    Remove Registrations Controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Kandan::Application.routes.draw do
   devise_for :users, :controllers => {
     :sessions => "sessions",
     :omniauth_callbacks => "users/omniauth_callbacks",
-    :registrations => "registrations"
   }
 
   root :to => "main#index"
